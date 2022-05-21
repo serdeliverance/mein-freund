0cdd7a6c4a690a90ca8cc9e51e9f5a1d62945862
Sergio Cano <canosergio90@gmail.com>
2022-05-21 13:51:03 -0300

    adding credit circe encoder
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/scala/io/github/sdev/meinfreund/adapters/in/rest/json/SerDes.scala
+++ src/main/scala/io/github/sdev/meinfreund/adapters/in/rest/json/SerDes.scala
@@ -2,15 +2,21 @@ package io.github.sdev.meinfreund.adapters.in.rest.json
 
 import io.circe.Decoder
 import io.circe.Encoder
-import io.circe.Printer
 import io.github.sdev.meinfreund.domain.entities.Credit
 import io.github.sdev.meinfreund.domain.entities.OriginalExpense
+import io.circe.generic.semiauto.deriveEncoder
 
 object SerDes:
-  given customPrinter: Printer = Printer.noSpaces.copy(dropNullValues = true)
 
-  // TODO
-  given creditEncoder: Encoder[Credit] = ???
+  object CreditFields:
+    val AMOUNT_USD      = "amount_usd"
+    val CONVERSION_RATE = "conversion_rate"
+    val PERIOD          = "period"
+
+  given creditEncoder: Encoder[Credit] =
+    Encoder.forProduct3(CreditFields.AMOUNT_USD, CreditFields.CONVERSION_RATE, CreditFields.PERIOD)(c =>
+      (c.amountUsd, c.conversionRate, c.period)
+    )
 
   // TODO
   given expenseDecoder: Decoder[OriginalExpense] = ???
