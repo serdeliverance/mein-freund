9af49f6263fb84a6ade9e43198b50203f97a0109
Sergio Cano <canosergio90@gmail.com>
2022-05-21 14:32:32 -0300

    category and expense decoder
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/scala/io/github/sdev/meinfreund/adapters/in/rest/json/SerDes.scala
+++ src/main/scala/io/github/sdev/meinfreund/adapters/in/rest/json/SerDes.scala
@@ -4,7 +4,8 @@ import io.circe.Decoder
 import io.circe.Encoder
 import io.github.sdev.meinfreund.domain.entities.Credit
 import io.github.sdev.meinfreund.domain.entities.OriginalExpense
-import io.circe.generic.semiauto.deriveEncoder
+import io.circe.generic.semiauto.deriveDecoder
+import io.github.sdev.meinfreund.domain.entities.Category
 
 object SerDes:
 
@@ -13,10 +14,14 @@ object SerDes:
     val CONVERSION_RATE = "conversion_rate"
     val PERIOD          = "period"
 
+  /*
+    Semiauto-manual definition in order to have snake case fields (because of circeGenericExtras issues crossBuilding scala 3)
+   */
   given creditEncoder: Encoder[Credit] =
     Encoder.forProduct3(CreditFields.AMOUNT_USD, CreditFields.CONVERSION_RATE, CreditFields.PERIOD)(c =>
       (c.amountUsd, c.conversionRate, c.period)
     )
 
-  // TODO
-  given expenseDecoder: Decoder[OriginalExpense] = ???
+  given categoryDecoder: Decoder[Category] = deriveDecoder
+
+  given expenseDecoder: Decoder[OriginalExpense] = deriveDecoder
