53ea7827d4309cf2194198107cc4727300a485d0
Sergio Cano <canosergio90@gmail.com>
2022-05-22 16:01:24 -0300

    removing unnecesary extention method
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/scala/io/github/sdev/meinfreund/application/util/ExpenseUtil.scala
+++ src/main/scala/io/github/sdev/meinfreund/application/util/ExpenseUtil.scala
@@ -6,10 +6,3 @@ import io.github.sdev.meinfreund.domain.entities.ExpenseId
 object ExpenseUtil:
 
   extension (date: LocalDateTime) def toExpensePeriod: String = s"${date.getYear}-${date.getMonth}"
-
-  // TODO think if this method makes sense here or should be an smart constructor of ExpenseId case class
-  extension (maybeId: Option[Long])
-    def toExpenseIdList: List[ExpenseId] =
-      maybeId match
-        case Some(expId) => List(ExpenseId(expId))
-        case None        => List.empty[ExpenseId]
--- src/test/scala/io/github/sdev/meinfreund/application/util/ExpenseUtilSpec.scala
+++ src/test/scala/io/github/sdev/meinfreund/application/util/ExpenseUtilSpec.scala
@@ -2,5 +2,4 @@ package io.github.sdev.meinfreund.application.util
 
 class ExpenseUtilSpec {
   // TODO test toExpensePeriod extenstion method
-  // TODO test toExpenseIdList
 }
